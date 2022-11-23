f35f64832e78c0d6a4cd59a66b574621afea24c0
GitHub Action <action@github.com>
2022-11-23 09:05:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/akiym/intellijcopylines/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/akiym/intellijcopylines/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.akiym.intellijcopylines.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.akiym.intellijcopylines.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/akiym/intellijcopylines/services/MyProjectService.kt
+++ src/main/kotlin/com/github/akiym/intellijcopylines/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.akiym.intellijcopylines.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.akiym.intellijcopylines.MyBundle
 
 class MyProjectService(project: Project) {
 
