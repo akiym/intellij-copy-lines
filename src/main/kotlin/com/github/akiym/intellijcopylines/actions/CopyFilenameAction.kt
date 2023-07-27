f88a3965a82f2d95f414ebd19ed89f98d4fcccc3
Takumi Akiyama <t.akiyama@flatt.tech>
2023-07-27 21:00:12 +0900

    add feature for copy project relative path

---
+++ src/main/kotlin/com/github/akiym/intellijcopylines/actions/CopyFilenameAction.kt
@@ -0,0 +1,23 @@
+package com.github.akiym.intellijcopylines.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.ide.CopyPasteManager
+import java.awt.datatransfer.StringSelection
+import kotlin.io.path.Path
+import kotlin.io.path.pathString
+
+class CopyFilenameAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val psiFile = e.getRequiredData(CommonDataKeys.PSI_FILE)
+        val basePath = e.project?.basePath
+        val file = if (basePath != null) {
+            Path(basePath).relativize(psiFile.virtualFile.toNioPath()).pathString
+        } else {
+            psiFile.virtualFile.path
+        }
+
+        CopyPasteManager.getInstance().setContents(StringSelection(file))
+    }
+}
